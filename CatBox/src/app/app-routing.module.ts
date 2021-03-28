b87c2e58a0a7de974988d98e9b7cc089bb3b20fd
MSahajdakiewicz <m.sahajdakiewicz@wp.pl>
2021-03-28 20:13:15 +0200

    CatBox

--- CatBox/src/app/app-routing.module.ts
+++ CatBox/src/app/app-routing.module.ts
@@ -15,7 +15,6 @@ const routes: Routes = [
     path: 'register',
     loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
   },
-
   {
     path: 'dashboard',
     loadChildren: () => import('./dashboard/dashboard.module').then( m => m.DashboardPageModule)
--- CatBox/src/app/app.component.ts
+++ CatBox/src/app/app.component.ts
@@ -1,34 +1,47 @@
 import { Component, OnInit } from '@angular/core';
-
-// Remeber the Imports
 import { Storage } from '@ionic/storage';
 import { Router } from '@angular/router';
-
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent implements OnInit{
+
+  //--------------------------------------------------------
+
+export class AppComponent implements OnInit {
+
   user = {}
-  constructor(private storage: Storage, private router: Router) {}
+
+  //--------------------------------------------------------
+
+  constructor(private storage: Storage, private router: Router) { }
+
+  //--------------------------------------------------------
 
   ngOnInit() {
-    // Get the user object
+
+  //---Get user details
     this.storage.get('user').then((obj) => {
       console.log(obj);
       this.user = obj
     });
   }
 
-  delete(){
+  //--------------------------------------------------------
+
+  //---Log out
+  logout() {
+    this.router.navigate(['/'])
+  }
+
+  //--------------------------------------------------------
+
+  //---Delete account
+  delete() {
     this.storage.set('user', null).then((obj) => {
       this.router.navigate(['/'])
     });
-  }
-
-  logout(){
-    this.router.navigate(['/'])
   }
 }
